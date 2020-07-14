30f8951044ad835c18579df95e9e52fb52c5bc74
jagdishkumawat30 <jagdish.kumawat28@gmail.com>
2020-07-14 23:48:09 +0530

    initial commit

---
+++ ModelClass.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CovidIndiaAPI
+{
+    class ModelClass
+    {
+        // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
+        
+
+        
+
+        
+
+
+    }
+
+    public class Root
+    {
+        public List<CasesTimeSery> cases_time_series { get; set; }
+        public List<Tested> tested { get; set; }
+
+    }
+
+    public class Tested
+    {
+        public string individualstestedperconfirmedcase { get; set; }
+        public string positivecasesfromsamplesreported { get; set; }
+        public string samplereportedtoday { get; set; }
+        public string source { get; set; }
+        public string source1 { get; set; }
+        public string testedasof { get; set; }
+        public string testpositivityrate { get; set; }
+        public string testsconductedbyprivatelabs { get; set; }
+        public string testsperconfirmedcase { get; set; }
+        public string testspermillion { get; set; }
+        public string totalindividualstested { get; set; }
+        public string totalpositivecases { get; set; }
+        public string totalsamplestested { get; set; }
+        public string updatetimestamp { get; set; }
+
+    }
+
+    public class CasesTimeSery
+    {
+        public string dailyconfirmed { get; set; }
+        public string dailydeceased { get; set; }
+        public string dailyrecovered { get; set; }
+        public string date { get; set; }
+        public string totalconfirmed { get; set; }
+        public string totaldeceased { get; set; }
+        public string totalrecovered { get; set; }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CovidIndiaAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myJsonResponse = GetCovidData().GetAwaiter().GetResult();
+            
+            Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+
+            Console.WriteLine(myDeserializedClass.cases_time_series[myDeserializedClass.cases_time_series.Count - 1].dailyconfirmed + " - Daily Confirmed");
+            Console.WriteLine("Total tested -" + myDeserializedClass.tested[myDeserializedClass.tested.Count - 1].totalsamplestested);
+        }
+
+        private static async Task<string> GetCovidData()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://api.covid19india.org/data.json");
+            client.DefaultRequestHeaders.Accept.Clear();
+
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, client.BaseAddress);
+
+            var response = await MakeRequestAsync(request, client);
+            return response;
+        }
+
+        public static async Task<string> MakeRequestAsync(HttpRequestMessage getRequest, HttpClient client)
+        {
+            var response = await client.SendAsync(getRequest).ConfigureAwait(false);
+            var responseString = string.Empty;
+            try
+            {
+                response.EnsureSuccessStatusCode();
+                responseString = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+            }
+            catch (HttpRequestException)
+            {
+
+            }
+
+            return responseString;
+        }
+    }
+}
